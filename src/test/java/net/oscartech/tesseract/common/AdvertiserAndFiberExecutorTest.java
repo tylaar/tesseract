53b13d7ec612fefaa210634062c8d38619363393
tylaar <tylaar@yeah.net>
2015-05-13 00:48:03 +0800

    add fiber advertiser test and default listening advertiser test

--- src/test/java/net/oscartech/tesseract/common/AdvertiserAndFiberExecutorTest.java
+++ src/test/java/net/oscartech/tesseract/common/AdvertiserAndFiberExecutorTest.java
@@ -1,13 +1,21 @@
 package net.oscartech.tesseract.common;
 
+import com.google.common.collect.Lists;
+import net.oscartech.tesseract.common.concurrent.Callback;
+import net.oscartech.tesseract.common.fiber.DefaultListeningAdvertiser;
 import net.oscartech.tesseract.common.fiber.FiberExecutor;
+import net.oscartech.tesseract.common.fiber.ListeningAdvertiser;
 import org.junit.After;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by tylaar on 15/5/12.
@@ -43,5 +51,41 @@ public class AdvertiserAndFiberExecutorTest {
         fiber.execute(decrementor);
         fiber.execute(decrementor);
         latch.await();
+    }
+
+    @Test(timeout = 10000)
+    public void testDefaultListeningAdvertiser() throws InterruptedException {
+        final ExecutorService fiber = new FiberExecutor(threadPool);
+        final ListeningAdvertiser<String> advertiser = new DefaultListeningAdvertiser<>();
+
+        final CountDownLatch latch = new CountDownLatch(3);
+        advertiser.addCallback(fiber, new Callback<String>() {
+            @Override
+            public void apply(final String message) {
+                if (message.equals("Hello")) {
+                    latch.countDown();
+                    latch.countDown();
+                } else if (message.equals("Goodbye")) {
+                    latch.countDown();
+                }
+            }
+        });
+
+        final CountDownLatch adderLatch = new CountDownLatch(2);
+        final List<String> messages = new CopyOnWriteArrayList<>();
+        advertiser.addCallback(fiber, new Callback<String>() {
+            @Override
+            public void apply(final String message) {
+                messages.add(message);
+                adderLatch.countDown();
+            }
+        });
+        assertEquals(3, latch.getCount());
+        advertiser.publish("Hello");
+        advertiser.publish("Goodbye");
+        latch.await();
+        adderLatch.await();
+        assertEquals(0, latch.getCount());
+        assertEquals(Lists.newArrayList("Hello", "Goodbye"), messages);
     }
 }
