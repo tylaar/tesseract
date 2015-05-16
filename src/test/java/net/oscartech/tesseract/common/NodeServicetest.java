7186c40c11bdb0afea1060fad4409fb0dec8b1c5
tylaar <tylaar@yeah.net>
2015-05-16 13:37:19 +0800

    node service scratch test

---
+++ src/test/java/net/oscartech/tesseract/common/NodeServicetest.java
@@ -0,0 +1,47 @@
+package net.oscartech.tesseract.common;
+
+import net.oscartech.tesseract.node.NodeService;
+import net.oscartech.tesseract.node.NodeServiceFactory;
+import net.oscartech.tesseract.node.pojo.NodeProposal;
+import net.oscartech.tesseract.node.rpc.aop.RpcServiceProcessor;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * Created by tylaar on 15/4/26.
+ */
+public class NodeServiceTest {
+    @Test
+    public void testNodeStartUp() throws InterruptedException, IOException {
+        List<NodeService> services =  NodeServiceFactory.generateSomeService();
+        ThreadFactory threadFactory = Executors.defaultThreadFactory();
+        //ThreadPoolExecutor executorPool = new ThreadPoolExecutor(2, 4, 10, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(2), threadFactory);
+        for (NodeService service : services) {
+            service.init();
+        }
+        NodeService service = services.get(0);
+        NodeProposal proposal = new NodeProposal();
+        proposal.setProposalId(NodeServiceFactory.getSequenceGenerator().generateSequence());
+        proposal.setType(0);
+        proposal.setNanoDuration(System.nanoTime());
+        Thread.sleep(2000);
+        service.sendMasterProposal();
+
+        while(true) {
+            Thread.yield();
+        }
+    }
+
+    @Test
+    public void testAnnotationRefine() throws IOException {
+        RpcServiceProcessor processor = new RpcServiceProcessor();
+        processor.scanAnnotation("net.yorkjr.jareau.blackjack");
+
+
+    }
+}
